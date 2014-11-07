e857277c3de85428764d4127c52388f7a78e9917
AaronShea <aaron@aaronshea.me>
2014-11-07 18:25:10 -0500

    Minor category change

--- Source/CEF/Classes/CEFUIComponent.h
+++ Source/CEF/Classes/CEFUIComponent.h
@@ -84,7 +84,7 @@ class UCEFUIComponent : public UActorComponent
 		UPROPERTY(EditAnywhere, Category = "View")
 		FString DefaultURL;
 
-		UFUNCTION(BlueprintCallable, Category = "UI|CEF|InintComponent")
+		UFUNCTION(BlueprintCallable, Category = "UI|CEF")
 		void initComponent();
 
 	private:

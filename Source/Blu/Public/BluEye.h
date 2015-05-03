c1cf132e97de3150fb4e1a67fcf5f1664c5fded9
AaronShea <aaron@aaronshea.me>
2015-05-03 13:54:34 -0400

    Comment Cleanup

--- Source/Blu/Public/BluEye.h
+++ Source/Blu/Public/BluEye.h
@@ -23,39 +23,39 @@ class BLU_API UBluEye : public UObject
 
 	GENERATED_UCLASS_BODY()
 
-	/* Initialize function, should be called after properties are set */
+	/** Initialize function, should be called after properties are set */
 	UFUNCTION(BlueprintCallable, Category = "Blu")
 		void init();
 
-	/* The default URL this UI component will load */
+	/** The default URL this UI component will load */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Blu")
 		FString DefaultURL;
 
-	/* Is this UI component current active? */
+	/** Is this UI component current active? */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Blu")
 		bool bEnabled;
 
-	/* Should this be rendered in game to be transparent? */
+	/** Should this be rendered in game to be transparent? */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Blu")
 		bool bIsTransparent;
 
-	/* Width of the view resolution */
+	/** Width of the view resolution */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Blu")
 		int32 Width;
 
-	/* Height of the view resolution */
+	/** Height of the view resolution */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Blu")
 		int32 Height;
 
-	/* Material that will be instanced to load UI texture into it */
+	/** Material that will be instanced to load UI texture into it */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Blu")
 		UMaterialInterface* BaseMaterial;
 
-	/* Name of parameter to load UI texture into material */
+	/** Name of parameter to load UI texture into material */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Blu")
 		FName TextureParameterName = "BluTexture";
 
-	/* Get the texture data from our UI component */
+	/** Get the texture data from our UI component */
 	UFUNCTION(BlueprintCallable, Category = "Blu")
 		UTexture2D* GetTexture() const;
 
@@ -63,30 +63,30 @@ class BLU_API UBluEye : public UObject
 	UFUNCTION(BlueprintCallable, Category = "Blu")
 		UMaterialInstanceDynamic* GetMaterialInstance() const;
 
-	/* Execute JS code inside the browser */
+	/** Execute JS code inside the browser */
 	UFUNCTION(BlueprintCallable, Category = "Blu")
 		void ExecuteJS(const FString& code);
 
 	UFUNCTION(BlueprintCallable, Category = "Blu")
 		void LoadURL(const FString& newURL);
 
-	/* Trigger a LEFT click in the browser via a Vector2D */
+	/** Trigger a LEFT click in the browser via a Vector2D */
 	UFUNCTION(BlueprintCallable, Category = "Blu")
 		void TriggerLeftClick(const FVector2D& pos, const float scale = 1);
 
-	/* Trigger a RIGHT click in the browser via a Vector2D */
+	/** Trigger a RIGHT click in the browser via a Vector2D */
 	UFUNCTION(BlueprintCallable, Category = "Blu")
 		void TriggerRightClick(const FVector2D& pos, const float scale = 1);
 
-	/* Trigger a LEFT MOUSE DOWN in the browser via a Vector2D */
+	/** Trigger a LEFT MOUSE DOWN in the browser via a Vector2D */
 	UFUNCTION(BlueprintCallable, Category = "Blu")
 		void TriggerLeftMouseDown(const FVector2D& pos, const float scale = 1);
 
-	/* Trigger a RIGHT MOUSE DOWN in the browser via a Vector2D */
+	/** Trigger a RIGHT MOUSE DOWN in the browser via a Vector2D */
 	UFUNCTION(BlueprintCallable, Category = "Blu")
 		void TriggerRightMouseDown(const FVector2D& pos, const float scale = 1);
 
-	/* Trigger a LEFT MOUSE DOWN in the browser via a Vector2D */
+	/** Trigger a LEFT MOUSE DOWN in the browser via a Vector2D */
 	UFUNCTION(BlueprintCallable, Category = "Blu")
 		void TriggerLeftMouseUp(const FVector2D& pos, const float scale = 1);
 
@@ -94,34 +94,35 @@ class BLU_API UBluEye : public UObject
 	UFUNCTION(BlueprintCallable, Category = "Blu")
 		void TriggerRightMouseUp(const FVector2D& pos, const float scale = 1);
 
-	/* Move the mouse in the browser */
+	/** Move the mouse in the browser */
 	UFUNCTION(BlueprintCallable, Category = "Blu")
 		void TriggerMouseMove(const FVector2D& pos, const float scale = 1);
 
-	/* Move the mouse in the browser */
+	/** Move the mouse in the browser */
 	UFUNCTION(BlueprintCallable, Category = "Blu")
 		void TriggerMouseWheel(const float MouseWheelDelta, const FVector2D& pos, const float scale = 1);
 
-	/* Javascript event emitter */
+	/** Javascript event emitter */
 	UPROPERTY(BlueprintAssignable)
 		FScriptEvent ScriptEventEmitter;
 
-	/* Trigger a key down event */
+	/** Trigger a key down event */
 	UFUNCTION(BlueprintCallable, Category = "Blu")
 		void KeyDown(FKeyEvent InKey);
 
-	/* Trigger a key down event */
+	/** Trigger a key down event */
 	UFUNCTION(BlueprintCallable, Category = "Blu")
 		void KeyUp(FKeyEvent InKey);
 
-	/* Trigger a key press event */
+	/** Trigger a key press event */
 	UFUNCTION(BlueprintCallable, Category = "Blu")
 		void KeyPress(FKeyEvent InKey);
 
-	/* Trigger a character key event */
+	/** Trigger a character key event */
 	UFUNCTION(BlueprintCallable, Category = "Blu")
 		void CharKeyPress(FCharacterEvent CharEvent);
 
+	/** Trigger a raw keypress via a character */
 	UFUNCTION(BlueprintCallable, Category = "Blu", meta = (AdvancedDisplay = "2"))
 		void RawCharKeyPress(const FString charToPress, bool isRepeat,
 								bool LeftShiftDown,
@@ -142,7 +143,7 @@ class BLU_API UBluEye : public UObject
 
 	void TextureUpdate(const void* buffer);
 
-	void BeginDestroy() override; //
+	void BeginDestroy() override;
 
 	protected:
 		CefWindowInfo info;

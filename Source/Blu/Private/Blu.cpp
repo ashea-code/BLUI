370af71f3af49d2520c429522bbf54fe0f052fe7
AaronShea <aaron@aaronshea.me>
2015-05-08 20:58:24 -0400

    Prevent audio from playing after browser is closed

--- Source/Blu/Private/Blu.cpp
+++ Source/Blu/Private/Blu.cpp
@@ -13,7 +13,6 @@ class FBlu : public IBlu
 		// Setup the default settings for BluManager
 		BluManager::settings.windowless_rendering_enabled = true;
 		BluManager::settings.no_sandbox = true;
-
 		BluManager::settings.remote_debugging_port = 7777;
 		
 #if PLATFORM_LINUX

199ae4b691a847aea21529f452fa1e23847765e5
AaronShea <aaron@aaronshea.me>
2015-05-11 19:06:27 -0400

    info on switches

--- Source/Blu/Private/BluManager.cpp
+++ Source/Blu/Private/BluManager.cpp
@@ -11,6 +11,10 @@ void BluManager::OnBeforeCommandLineProcessing(const CefString& process_type,
 	command_line->AppendSwitch("off-screen-rendering-enabled");
 	command_line->AppendSwitch("in-process-gpu");
 	command_line->AppendSwitchWithValue("use-gl", "osmesa");
+
+	// Append more command line options here if you want
+	// Visit Peter Beverloo's site: http://peter.sh/experiments/chromium-command-line-switches/ for more info on the switches
+
 }
 
 void BluManager::doBluMessageLoop()

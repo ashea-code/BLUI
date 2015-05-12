856e021d753da8a262df9c35a8b5b6a9feb3640b
AaronShea <aaron@aaronshea.me>
2015-05-11 20:00:59 -0400

    Default CPU saver config
    Add more switches

--- Source/Blu/Private/BluManager.cpp
+++ Source/Blu/Private/BluManager.cpp
@@ -8,9 +8,36 @@ BluManager::BluManager()
 void BluManager::OnBeforeCommandLineProcessing(const CefString& process_type,
 	CefRefPtr< CefCommandLine > command_line)
 {
+
+	/////////////////
+	/**
+	* Used to pick command line switches
+	* If set to "true": CEF will use less CPU, but rendering performance will be lower. CSS3 and WebGL may not be usable
+	* If set to "false": CEF will use more CPU, but rendering will be better, CSS3 and WebGL will also be usable
+	*/
+	BluManager::CPURenderSettings = true;
+	/////////////////
+
 	command_line->AppendSwitch("off-screen-rendering-enabled");
-	command_line->AppendSwitch("in-process-gpu");
-	command_line->AppendSwitchWithValue("use-gl", "osmesa");
+	command_line->AppendSwitchWithValue("off-screen-frame-rate", "60");
+	command_line->AppendSwitch("enable-font-antialiasing");
+	command_line->AppendSwitch("enable-media-stream");
+
+	// Should we use the render settings that use less CPU?
+	if (CPURenderSettings)
+	{
+		command_line->AppendSwitch("disable-gpu");
+		command_line->AppendSwitch("disable-gpu-compositing");
+		command_line->AppendSwitch("enable-begin-frame-scheduling");
+	}
+	else
+	{
+		// Enables things like CSS3 and WebGL
+		command_line->AppendSwitch("in-process-gpu");
+		command_line->AppendSwitchWithValue("use-gl", "osmesa");
+		command_line->AppendSwitch("enable-gpu-rasterization");
+	}
+
 
 	// Append more command line options here if you want
 	// Visit Peter Beverloo's site: http://peter.sh/experiments/chromium-command-line-switches/ for more info on the switches
@@ -24,3 +51,4 @@ void BluManager::doBluMessageLoop()
 
 CefSettings BluManager::settings;
 CefMainArgs BluManager::main_args;
+bool BluManager::CPURenderSettings = true;

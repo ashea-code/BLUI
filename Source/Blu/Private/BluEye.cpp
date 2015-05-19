da5abeab8d7ad29f2e715edc46f9caf833c74315
AaronShea <aaron@aaronshea.me>
2015-05-19 13:47:37 -0400

    Clean up key char method

--- Source/Blu/Private/BluEye.cpp
+++ Source/Blu/Private/BluEye.cpp
@@ -344,15 +344,10 @@ void UBluEye::CharKeyPress(FCharacterEvent CharEvent)
 	processKeyMods(CharEvent);
 
 	// Below char input needs some special treatment, se we can't use the normal key down/up methods
+
 	key_event.windows_key_code = CharEvent.GetCharacter();
 	key_event.native_key_code = CharEvent.GetCharacter();
 	key_event.type = KEYEVENT_CHAR;
-	browser->GetHost()->SendKeyEvent(key_event);
-
-	key_event.windows_key_code = CharEvent.GetCharacter();
-	key_event.native_key_code = CharEvent.GetCharacter();
-
-	key_event.type = KEYEVENT_KEYUP;
 	browser->GetHost()->SendKeyEvent(key_event);
 
 }

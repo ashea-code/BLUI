fd9cffc58e2cc51e51f672a2a8a6bb84f5a66cf4
AaronShea <aaron@aaronshea.me>
2015-07-06 10:13:21 -0400

    Load local DLLs on windows

--- Source/BluLoader/Private/BluLoader.cpp
+++ Source/BluLoader/Private/BluLoader.cpp
@@ -6,18 +6,23 @@ class FBluLoader : public IBluLoader
 	/** IModuleInterface implementation */
 	virtual void StartupModule() override
 	{
-		UE_LOG(LogBluLoader, Log, TEXT("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! STATUS: Loaded"));
+		FString LibPath = FPaths::ConvertRelativePathToFull(FPaths::GameDir() + "Plugins/BLUI/ThirdParty/cef/");
+
+		// If we're on Windows in Editor we need to add a new path to load DLLs from
+		#if PLATFORM_WINDOWS && WITH_EDITOR
+			LibPath += "Win/shipping/";
+			SetDllDirectory(*LibPath);
+		#endif
+
+		UE_LOG(LogBluLoader, Log, TEXT("STATUS: BLUI Ready to Load!"));
 	}
 
 	virtual void ShutdownModule() override
 	{
-		UE_LOG(LogBluLoader, Log, TEXT("STATUS: Shutdown"));
+		UE_LOG(LogBluLoader, Log, TEXT("STATUS: BLUI Has Shutdown"));
 	}
 
 };
-
-
-
 
 IMPLEMENT_MODULE( FBluLoader, BluLoader )
 DEFINE_LOG_CATEGORY(LogBluLoader);
